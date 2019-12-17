f9f58aad105c7a4126e5565df687fbcde5c476ea
cxm <eraser@bytuetech.com>
2019-12-17 14:35:49 +0800

    String 字符串的截取方法：

---
+++ day08-code/src/com/cxm/day08/domo02/Demo03Sbustring.java
@@ -0,0 +1,28 @@
+package com.cxm.day08.domo02;
+/*
+String
+字符串的截取方法：
+
+public String substring(int index)：截取从参数位置一直到字符串末尾，返回新字符串。
+public String substring(int begin, int end)：截取从begin开始，一直到end结束，中间的字符串。
+备注：[begin,end)，包含左边，不包含右边。
+ */
+public class Demo03Sbustring {
+    public static void main(String[] args) {
+        String str1 = "HelloWorld!";
+        String str2 = str1.substring(5);
+        System.out.println(str1);
+        System.out.println(str2);
+        System.out.println("=================");
+
+        String str3 = str1.substring(4,7);
+        System.out.println(str3);
+        System.out.println("===================");
+
+        String strA = "Hello";
+        System.out.println(strA);
+        strA = "java";
+        System.out.println(strA);
+
+    }
+}
