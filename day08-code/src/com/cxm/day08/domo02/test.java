f3c0461486359d49f1640b1da8df78d2d8aaf5de
cxm <eraser@bytuetech.com>
2019-12-17 14:08:49 +0800

    aaa

--- day08-code/src/com/cxm/day08/domo02/test.java
+++
@@ -1,9 +0,0 @@
-package com.cxm.day08.domo02;
-
-public class test {
-    public static void main(String[] args) {
-        System.out.println("=================");
-        System.out.println("==============================");
-        System.out.println("qqqqqqqqqqqq");
-    }
-}
