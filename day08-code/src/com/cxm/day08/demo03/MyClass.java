f39d0006aebed66eac687680d76a20e75339cbd4
cxm <eraser@bytuetech.com>
2019-12-17 17:59:57 +0800

    static修饰成员方法

---
+++ day08-code/src/com/cxm/day08/demo03/MyClass.java
@@ -0,0 +1,10 @@
+package com.cxm.day08.demo03;
+
+public class MyClass {
+    public void method(){
+        System.out.println("成员方法");
+    }
+    public static void methodStatic(){
+        System.out.println("静态方法");
+    }
+}
