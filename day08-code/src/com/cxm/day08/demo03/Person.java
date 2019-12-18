555e285a382a774073a11f86bc15533d0cdca3e1
cxm <eraser@bytuetech.com>
2019-12-18 11:47:36 +0800

    静态代码块 特点：当第一次用到本类时，静态代码块执行唯一的一次。

---
+++ day08-code/src/com/cxm/day08/demo03/Person.java
@@ -0,0 +1,13 @@
+package com.cxm.day08.demo03;
+/*
+静态代码块
+特点：当第一次用到本类时，静态代码块执行唯一的一次。
+ */
+public class Person {
+    static {
+        System.out.println("静态代码块");
+    }
+    public Person(){
+        System.out.println("构造方法执行");
+    }
+}
