c946c7559c2a82d016abf7180eb3c6aa86bc045f
cxm <eraser@bytuetech.com>
2019-12-18 13:15:41 +0800

    Arrays练习

---
+++ day08-code/src/com/cxm/day08/demo04/Demo02ArraysPractise.java
@@ -0,0 +1,20 @@
+package com.cxm.day08.demo04;
+
+import java.util.Arrays;
+
+/*
+题目：
+请使用Arrays相关的API，将一个随机字符串中的所有字符升序排列，并倒序打印。
+ */
+public class Demo02ArraysPractise {
+    public static void main(String[] args) {
+        String str = "asdfweradsfghjklzxcvbnm";
+
+        char[] chars = str.toCharArray();
+        Arrays.sort(chars);
+
+        for (int i = chars.length - 1; i >= 0; i--) {
+            System.out.print(chars[i]);
+        }
+    }
+}
