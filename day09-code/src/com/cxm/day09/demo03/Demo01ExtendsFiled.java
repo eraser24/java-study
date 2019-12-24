de344490773536a269e32f783c87509cd0253348
erase24 <1977719249@qq.com>
2019-12-24 20:32:22 +0800

    Extends 继承中成员的方法访问特点

---
+++ day09-code/src/com/cxm/day09/demo03/Demo01ExtendsFiled.java
@@ -0,0 +1,13 @@
+package com.cxm.day09.demo03;
+/*
+Extends 继承中成员的方法访问特点
+局部变量：       直接写成员变量名
+本类的成员变量：  this.成员变量名
+父类的成员变量：  super.成员变量名
+ */
+public class Demo01ExtendsFiled {
+    public static void main(String[] args) {
+        Zi zi = new Zi();
+        zi.method();
+    }
+}
