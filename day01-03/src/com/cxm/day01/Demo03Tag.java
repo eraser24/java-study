5d4d0b1c6d1d021c4af3aa8eeacd56091e96101b
cxm <eraser@bytuetech.com>
2020-01-12 09:58:56 +0800

    标识符   类、变量、方法名规范

---
+++ day01-03/src/com/cxm/day01/Demo03Tag.java
@@ -0,0 +1,18 @@
+package com.cxm.day01;
+/*
+标识符：
+    命名规则：
+        1、可以包含字母、数字、符号、下划线
+        2、不能以数字开头
+        3、不能是关键字
+    命名规范：
+        1、类名称：首字母大写，后面每个单词首字母大写（大驼峰）
+        2、变量名规范：首字母小写，后面每个大词首字母大写（小驼峰）
+        3、方法名规范：同变量名
+
+ */
+public class Demo03Tag {
+    public static void main(String[] args) {
+        System.out.println(" 你 好 ！");
+    }
+}
