709912e72782452b7291ce054344346604d9b20d
cxm <eraser@bytuetech.com>
2020-01-12 16:39:46 +0800

    switch语句使用的注意事项： 穿透case

---
+++ day03-code/src/com/cxm/day03/Demo08SwitchNotice.java
@@ -0,0 +1,13 @@
+package com.cxm.day03;
+/*
+switch语句使用的注意事项：
+1、多个case后边的数值不可以重复。
+2、switch后边小括号内只能是
+    基本数据类型：byte、short、char、int
+    引用数据类型：String字符串、enum枚举
+3、switch语句格式可以很灵活：前后顺序可以颠倒，而且breek可以省略
+”满足匹配哪一个case就从哪一个位置想下执行，知道遇到break结束为止“--穿透case
+ */
+public class Demo08SwitchNotice {
+
+}
