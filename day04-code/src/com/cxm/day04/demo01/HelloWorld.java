e2d171ef45850493858a5d6e681a6200a96026ab
erase24 <1977719249@qq.com>
2020-01-12 21:59:26 +0800

    方法复习

--- day04-code/src/com/cxm/day04/demo01/HelloWorld.java
+++ day04-code/src/com/cxm/day04/demo01/HelloWorld.java
@@ -1,5 +1,7 @@
 package com.cxm.day04.demo01;
 
+import java.util.Scanner;
+
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!111");
--- day04-code/src/com/cxm/day04/demo02/Demo01Method.java
+++ day04-code/src/com/cxm/day04/demo02/Demo01Method.java
@@ -1,4 +1,5 @@
 package com.cxm.day04.demo02;
+
 /*
 定义格式
 public static void main() {
@@ -14,14 +15,15 @@ public static void main() {
 * */
 public class Demo01Method {
     public static void main(String[] args) {
-        printMethod();
+        Method();
+    }
+
+    public static void Method() {
+        for (int heng = 0; heng < 5; heng++) {
+            for (int shu = 0; shu < 20; shu++) {
+                System.out.print("*");
             }
-            public static void printMethod () {
-                for (int j = 0; j < 5; j++) {
-                    for (int i = 0; i < 20; i++) {
-                        System.out.print("*");
-            }
-                    System.out.println();
+            System.out.println("");
         }
     }
 
