2b5d169e1518cc1a89ed6dd554b5f0c13bb635b5
cxm <eraser@bytuetech.com>
2020-01-12 15:56:36 +0800

    标准的if else语句

---
+++ day03-code/src/com/cxm/day03/Demo03IfElse.java
@@ -0,0 +1,16 @@
+package com.cxm.day03;
+
+/*
+标准if  else语句
+
+ */
+public class Demo03IfElse {
+    public static void main(String[] args) {
+        int num = 666;
+        if (num % 2 == 0) {
+            System.out.println("偶数");
+        } else {
+            System.out.println("基数");
+        }
+    }
+}
