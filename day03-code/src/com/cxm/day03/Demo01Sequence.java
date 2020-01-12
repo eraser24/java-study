c7d718581c66f50a58a6f842e4f640b559edc302
cxm <eraser@bytuetech.com>
2020-01-12 15:48:15 +0800

    顺序结构

---
+++ day03-code/src/com/cxm/day03/Demo01Sequence.java
@@ -0,0 +1,12 @@
+package com.cxm.day03;
+/*
+顺序结构 sequence
+ */
+public class Demo01Sequence {
+    public static void main(String[] args) {
+        System.out.println("今天天气不错");
+        System.out.println("风和日丽");
+        System.out.println("下午没课");
+        System.out.println("爽");
+    }
+}
