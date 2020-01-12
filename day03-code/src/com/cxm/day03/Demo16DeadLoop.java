0a7a914dcbcf892fb8dfc04cd818f4faa51dc7f2
cxm <eraser@bytuetech.com>
2020-01-12 17:34:45 +0800

    死循环

---
+++ day03-code/src/com/cxm/day03/Demo16DeadLoop.java
@@ -0,0 +1,19 @@
+package com.cxm.day03;
+
+import javax.swing.*;
+
+/*
+死循环
+永远停不下来的循环，叫做死循环
+
+死循环的标准格式：
+while（true）{
+    循环体
+}
+
+ */
+public class Demo16DeadLoop {
+    public static void main(String[] args) {
+
+    }
+}
