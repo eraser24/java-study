f15c1e47f89a1f08b74b17bbfa6cadcef7570413
cxm <eraser@bytuetech.com>
2020-01-12 17:39:03 +0800

    嵌套循环

---
+++ day03-code/src/com/cxm/day03/Demo17LoopHourAndMinute.java
@@ -0,0 +1,13 @@
+package com.cxm.day03;
+/*
+嵌套循环
+ */
+public class Demo17LoopHourAndMinute {
+    public static void main(String[] args) {
+        for (int hour = 0 ; hour < 24;hour++){
+            for (int minute = 0;minute<60;minute++) {
+                System.out.println(hour + "点" + minute + "分");
+            }
+        }
+    }
+}
