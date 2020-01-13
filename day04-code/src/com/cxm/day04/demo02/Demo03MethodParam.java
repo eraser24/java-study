3552327e213273513eba78f05aae98718ab9194d
cxm <eraser@bytuetech.com>
2020-01-13 10:50:50 +0800

    有参数，无参数方法

--- day04-code/src/com/cxm/day04/demo02/Demo03MethodParam.java
+++ day04-code/src/com/cxm/day04/demo02/Demo03MethodParam.java
@@ -17,7 +17,7 @@ public class Demo03MethodParam {
     }
     //打印10次
     public static void method2(){
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 11; i++) {
             System.out.println("Hello World" + i);
         }
     }
