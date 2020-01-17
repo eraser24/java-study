81a592db86f366ad374baa6e7a3457639e21fdd2
cxm <eraser@bytuetech.com>
2020-01-17 10:49:00 +0800

    数组--异常--空指针异常--exception--NullPointerException

--- day05-code/src/com/cxm/day05/demo03/Demo02ArrayNull.java
+++ day05-code/src/com/cxm/day05/demo03/Demo02ArrayNull.java
@@ -4,12 +4,11 @@ package com.cxm.day05.demo03;
 如果赋值null，没有new创建
 会发生：空指针异常  NullPointerException
 
-
  */
 public class Demo02ArrayNull {
     public static void main(String[] args) {
         int[] array = null;
-        array = new int[3];
+//        array = new int[3];
         System.out.println(array[0]);
     }
 }
