8eed9065cde753a59afcb40f5d05177434cc7940
cxm <eraser@bytuetech.com>
2020-01-17 13:13:08 +0800

    数组--数组作为返回值

--- day05-code/src/com/cxm/day05/demo04/Demo02ArrayReturn.java
+++ day05-code/src/com/cxm/day05/demo04/Demo02ArrayReturn.java
@@ -1,4 +1,5 @@
 package com.cxm.day05.demo04;
+
 /*
 一个方法有0,1多个参数，但是只能0,1个返回值，不能有多个返回值
 
@@ -8,16 +9,18 @@ package com.cxm.day05.demo04;
  */
 public class Demo02ArrayReturn {
     public static void main(String[] args) {
-        int[] result = calculate(10, 20,30);
+        int[] result = calculate(10, 20, 30);
         System.out.println(result[0]);
         System.out.println(result[1]);
     }
-    public static int[]  calculate(int a , int b ,int c ) {
-        int sum = a + b + c ;
+
+    public static int[] calculate(int a, int b, int c) {
+        int sum = a + b + c;
         int avg = sum / 3;
-        int[] array = new int[2];
-        array[0] = sum;
-        array[1] = avg;
+        int[] array = {sum, avg};
+//        int[] array = new int[2];
+//        array[0] = sum;
+//        array[1] = avg;
         return array;
     }
 }
