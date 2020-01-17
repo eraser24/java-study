a4ab46c83a8aff29b97302214ccadedec6cf9423
cxm <eraser@bytuetech.com>
2020-01-17 10:56:08 +0800

    数组--遍历打印

--- day05-code/src/com/cxm/day05/demo03/Demo04Array.java
+++ day05-code/src/com/cxm/day05/demo03/Demo04Array.java
@@ -1,4 +1,5 @@
 package src.com.cxm.day05.demo03;
+
 /*
 遍历数组，挨个处理，打印
  */
@@ -8,5 +9,12 @@ public class Demo04Array {
         for (int i = 0; i < array.length; i++) {
             System.out.println(array[i]);
         }
+        System.out.println("===============");
+        int[] arrayA = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        for (int i = 0; i < arrayA.length; i++) {
+            System.out.println(arrayA[i]);
+        }
+
+
     }
 }
