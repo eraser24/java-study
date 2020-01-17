4ef8e420f9124e27c7fcac13c147ab5e8291dbc5
cxm <eraser@bytuetech.com>
2020-01-17 13:46:44 +0800

    面向对象--创建成员变量，方法

--- day06-code/src/com/cxm/day06/demo01/Student.java
+++ day06-code/src/com/cxm/day06/demo01/Student.java
@@ -1,4 +1,5 @@
 package src.com.cxm.day06.demo01;
+
 /*
 定义一个类，用来模拟“学生”事物，其中就有两个组成部分。
 
@@ -29,13 +30,15 @@ public class Student {
     int age;//年龄
 
     //成员方法
-    public void eat(){
+    public void eat() {
         System.out.println("吃饭");
     }
-    public void sleep(){
+
+    public void sleep() {
         System.out.println("睡觉");
     }
-    public void study(){
+
+    public void study() {
         System.out.println("学习");
     }
 }
