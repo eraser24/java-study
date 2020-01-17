a7f177ff28863167169087a64e06b11977c242ab
cxm <eraser@bytuetech.com>
2020-01-17 09:33:51 +0800

    数组--静态初始化数组

--- day05-code/src/com/cxm/day05/demo01/Demo02Array.java
+++ day05-code/src/com/cxm/day05/demo01/Demo02Array.java
@@ -1,12 +1,15 @@
 package src.com.cxm.day05.demo01;
+
 /*
 动态指定长度
 静态初始化，指定内容
  */
 public class Demo02Array {
     public static void main(String[] args) {
-        int[] arrayA = new int[] { 5,15,25 };
-        String[] arrayB = new String[] { "hello","world","java"};
+        int[] arrayA = new int[]{5, 15, 25};
+        String[] arrayB = new String[]{"hello", "world", "java"};
 
+        int[] arrayA = new int[]{5, 15, 25};
+        String[] arrayB = new String[]{"hewll", "wworld", "java"};
     }
 }
