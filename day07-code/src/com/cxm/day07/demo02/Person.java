cf4d7b9e139e600400f5ade2e9716b55f76ed3dd
cxm <eraser@bytuetech.com>
2020-01-19 14:45:20 +0800

    1

--- day07-code/src/com/cxm/day07/demo02/Person.java
+++ day07-code/src/com/cxm/day07/demo02/Person.java
@@ -3,7 +3,7 @@ package com.cxm.day07.demo02;
 public class Person {
     String name;
 
-    public void showName(){
+    public void showName() {
         System.out.println("my name is " + name);
     }
 }
