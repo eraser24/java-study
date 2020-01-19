9ae7759b444b4902e8d92444c4953cb8f8f89004
cxm <eraser@bytuetech.com>
2020-01-19 17:45:16 +0800

    1

--- day09-code/src/com/cxm/day09/demo02/Zi.java
+++ day09-code/src/com/cxm/day09/demo02/Zi.java
@@ -1,11 +1,11 @@
 package com.cxm.day09.demo02;
 
-public class Zi extends  Fu{
+public class Zi extends Fu {
     int numZi = 20;
 
     int num = 200;
 
-    public void methodZi(){
+    public void methodZi() {
         //使用本类的num
         System.out.println(num);
     }
