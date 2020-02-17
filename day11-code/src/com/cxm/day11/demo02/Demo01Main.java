292fa5bf5efd7e447dcca8dbaef419b36cb544d9
erase24 <1977719249@qq.com>
2020-02-17 22:33:19 +0800

    四种权限修饰符

---
+++ day11-code/src/com/cxm/day11/demo02/Demo01Main.java
@@ -0,0 +1,13 @@
+package com.cxm.day11.demo02;
+/*
+java中有四种权限修饰符；
+                    public  > protected > (default) > private
+同一个类(我自己)        yes         yes         yes         yes
+同一个包（我邻居）      yes         yes         yes         no
+不同包（我儿子）        yes         yes         no           no
+不同包，非子类（陌生人） yes         no          no          no
+
+注意事项：（default）并不是关键字default，而是不写
+ */
+public class Demo01Main {
+}
---
+++ day11-code/src/com/cxm/day11/demo02/sub/Myson.java
@@ -0,0 +1,9 @@
+package com.cxm.day11.demo02.sub;
+
+import com.cxm.day11.demo02.MyClass;
+
+public class Myson extends MyClass {
+    public void methodSon(){
+        System.out.println(super.num);
+    }
+}
